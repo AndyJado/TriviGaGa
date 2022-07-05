f32011dca8e3709393474363de065145f1b47237
AndyJado <101876416+AndyJado@users.noreply.github.com>
2022-07-05 16:11:58 +0800

    contactList,写不动呢

---
+++ 吹微相机/Utilities/CantacList.swift
@@ -0,0 +1,20 @@
+//
+//  CantacList.swift
+//  TriviGaGa
+//
+//  Created by Andy Jado on 2022/7/5.
+//
+
+import SwiftUI
+
+actor ContactList {
+    
+    var codeOnes = [String : String]()
+    
+    func changeCode() {
+        
+    }
+    
+    
+    
+}
